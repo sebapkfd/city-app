b1d33f6109dc66292417fdbff2a1fa18311edad0
Sebastian Soto <sebastian.pkfd@gmail.com>
2024-06-06 01:53:45 -0400

    Installed carousel and added styles to issue page

--- src/components/IssuePage.tsx
+++ src/components/IssuePage.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import useEmblaCarousel from "embla-carousel-react";
 
 type Issue = {
   id: number;
@@ -12,6 +13,7 @@ type Issue = {
 
 const IssuePage = () => {
   const [data, setData] = useState<Issue | null>(null);
+  const [emblaRef] = useEmblaCarousel();
   let { id } = useParams();
 
   const getData = async () => {
@@ -37,18 +39,36 @@ const IssuePage = () => {
   }, []);
 
   return data ? (
-    <div>
+    <div className="issue-page">
       <Link to={"/"}>
         <button>Volver a Home</button>
       </Link>
-      <h1>{data.title}</h1>
-      <p>{data.date}</p>
-      {data.pictures.map((pic, i) => (
-        <img src={pic} key={data.title + "-" + i} />
-      ))}
-      <div>
-        <p>{data.description}</p>
-        <p>{data.location}</p>
+      <div className="main-row">
+        <div>
+          <h1 className="issue-page-title">{data.title}</h1>
+          <p className="issue-page-title">{data.date}</p>
+        </div>
+
+        <p className="issue-page-description">{data.description}</p>
+
+        <div className="embla" ref={emblaRef}>
+          <div className="embla__container">
+            {data.pictures.map((pic, i) => (
+              <div className="embla__slide" key={data.title + "-" + i}>
+                <img src={pic} />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* <div className="background">
+          {data.pictures.map((pic, i) => (
+            <img src={pic} key={data.title + "-" + i} />
+          ))}
+        </div> */}
+        <div>
+          <p>{data.location}</p>
+        </div>
       </div>
     </div>
   ) : null;
