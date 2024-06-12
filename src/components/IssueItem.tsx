7c38b1f436970fd508b169b788a96ce09978d92b
Seba <sebastian@workon.law>
2024-06-11 23:38:47 -0400

    Improved IssueItem card style

--- src/components/IssueItem.tsx
+++ src/components/IssueItem.tsx
@@ -10,14 +10,12 @@ type Issue = {
 const IssueItem = ({ issue }: { issue: Issue }) => {
   return (
     <div className="issue-item">
-      <div className="issue-header">
-        <div className="issue-title">
-          <h2>{issue.title}</h2>
-        </div>
-        <small>{issue.date}</small>
-      </div>
       <div className="issue-picture">
         <img src={"http://localhost:5173/" + issue.pictures[0]} />
+      </div>
+      <div className="issue-title">
+        <h2>{issue.title}</h2>
+        <p>{issue.date}</p>
       </div>
     </div>
   );
