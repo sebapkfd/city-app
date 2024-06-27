5e100daee3909d69796c4b0a9bb4e371859e3387
Seba <sebastian@workon.law>
2024-06-26 21:25:33 -0400

    First try with HashRouter

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://sebapkfd.github.io/city-app"
+  base: "/city-app/",
+  plugins: [react()]
 })
